c3b2a85b5fad91e53b90320a98ff1377a7dc6a50
Pratik Dhende <pratikdhende2000@gmail.com>
2025-03-01 22:51:07 -0800

    Add gamma correction

--- src/Color.h
+++ src/Color.h
@@ -7,10 +7,21 @@
 
 using Color = Vec3;
 
+static inline double linearToGamma(const double linearValue) {
+    if (linearValue > 0.0) {
+        return std::sqrt(linearValue);
+    }
+    return 0.0;
+}
+
 void write_color(std::ostream& out, const Color& pixel_color) {
     auto r = pixel_color.r();
     auto g = pixel_color.g();
     auto b = pixel_color.b();
+
+    r = linearToGamma(r);
+    g = linearToGamma(g);
+    b = linearToGamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     constexpr Interval interval(0.0, 0.999);
